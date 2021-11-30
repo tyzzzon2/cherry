116133ddaf2e976f0277e6599028ae41d60b42bf
evgeniy.chalenko <evgeniy.chalenko@managinglife.com>
2021-11-30 20:26:34 +0700

    Added createStringsFromArrays function for future use

--- additionalScriptThatBecameRedundant.php
+++ additionalScriptThatBecameRedundant.php
@@ -1,3 +1,14 @@
 <?php
 
 //some functionality
+
+function createStringsFromArrays (array $leftPartArray, array $rightPartArray) {
+    if (count($leftPartArray) != count($rightPartArray)) {
+        return ['Wrong parameters'];
+    }
+    $resultArray = [];
+    foreach ($leftPartArray as $value) {
+        $resultArray[] = $value . ', ' . array_shift($rightPartArray);
+    }
+    return $resultArray;
+}
